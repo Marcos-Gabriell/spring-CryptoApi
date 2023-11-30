b3841e1e7c1338a1ada882e43381fae857c8ae04
Marcos <marcosgabriel79355@gmail.com>
2023-11-30 18:57:15 -0300

    Avaçando no mapeamneto

--- src/main/java/CryptoApp/cryptoApp/repository/CoinRepository.java
+++ src/main/java/CryptoApp/cryptoApp/repository/CoinRepository.java
@@ -1,4 +1,12 @@
 package CryptoApp.cryptoApp.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class CoinRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public CoinRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
