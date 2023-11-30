233cf9bacbcc9104d934f79c2a4a903a63181194
Marcos <marcosgabriel79355@gmail.com>
2023-11-30 13:42:24 -0300

    Initial commit

---
+++ src/main/java/CryptoApp/cryptoApp/CryptoAppApplication.java
@@ -0,0 +1,13 @@
+package CryptoApp.cryptoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAppApplication.class, args);
+	}
+
+}
