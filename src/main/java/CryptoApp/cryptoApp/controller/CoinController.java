af95e6657bcef380751684b9d74328f047688106
Marcos <marcosgabriel79355@gmail.com>
2023-12-01 08:14:23 -0300

    Erro ao chamar método set

--- src/main/java/CryptoApp/cryptoApp/controller/CoinController.java
+++ src/main/java/CryptoApp/cryptoApp/controller/CoinController.java
@@ -2,6 +2,8 @@ package CryptoApp.cryptoApp.controller;
 
 import CryptoApp.cryptoApp.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,17 @@ public class CoinController {
 
     @Autowired
     private CoinRepository coinRepository;
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody Coin coin) {
+
+        try {
+            coin.set
+        }catch(Exceptionxe error) {
+
+        }
+    }
+
+
+
 }
