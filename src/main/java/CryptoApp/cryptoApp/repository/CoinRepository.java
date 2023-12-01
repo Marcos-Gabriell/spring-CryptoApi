2625e420573d5b15280bc9702c2ea71f462db18c
Marcos <marcosgabriel79355@gmail.com>
2023-12-01 10:10:49 -0300

    Implementando DTO para tranferencia de dados

--- src/main/java/CryptoApp/cryptoApp/repository/CoinRepository.java
+++ src/main/java/CryptoApp/cryptoApp/repository/CoinRepository.java
@@ -1,10 +1,17 @@
 package CryptoApp.cryptoApp.repository;
 
+import CryptoApp.cryptoApp.dto.CoinDTO;
 import CryptoApp.cryptoApp.entity.Coin;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class CoinRepository {
@@ -12,6 +19,7 @@ public class CoinRepository {
     private static final Logger logger = LoggerFactory.getLogger(CoinRepository.class);
 
     private static final String INSERT = "INSERT INTO coin (name, price, quantity, datetime) VALUES (?, ?, ?, ?)";
+    private static final String SELECT_ALL = "SELECT name, quantity FROM coin";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -20,7 +28,6 @@ public class CoinRepository {
     }
 
     public Coin insert(Coin coin) {
-
             Object[] attr = new Object[] {
                     coin.getName(),
                     coin.getPrice(),
@@ -29,5 +36,21 @@ public class CoinRepository {
             };
             jdbcTemplate.update(INSERT, attr);
             return coin;
+
+    }
+
+    public List<CoinDTO> getAll(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<CoinDTO>() {
+
+            @Override
+            public CoinDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                CoinDTO coin = new CoinDTO();
+                coin.setName(rs.getString("name"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+
+                return coin;
+            }
+        });
     }
 }
