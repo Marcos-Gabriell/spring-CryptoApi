5bf044ea9ecc91f918a380c4d706ecdeb3fcf216
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-01-22 18:06:22 -0300

    Avançando no front

--- src/main/resources/static/vueApp.js
+++ src/main/resources/static/vueApp.js
@@ -4,7 +4,15 @@ const baseUrl = "http://localhost:8081/coin";
 const mainContainer = {
     data() {
         return {
-            coins: []
+            coins: [],
+            formCoin: {
+                isNew: true,
+                name: '',
+                price: '',  // Corregido el nombre del atributo de precio
+                quantity: '',
+                title: 'Cadastrar nova transação',
+                button: 'Cadastrar'
+            }
         };
     },
     mounted() {
@@ -13,18 +21,47 @@ const mainContainer = {
 
     methods: {
         showAllCoins() {
-            axios.get(baseUrl)
+            axios
+                .get(baseUrl)
                 .then(response => {
-                    response.data.forEach(item => {
-                        this.coins.push({
-                            name: item.name,
-                            quantity: item.quantity
-                        });
-                    });
+                    this.coins = response.data;  // Simplificado para asignar directamente la respuesta al array coins
                 })
                 .catch(error => {
                     console.error("Error fetching coins:", error);
                 });
+        },
+        saveCoin() {
+            this.formCoin.name = this.formCoin.name.toUpperCase()
+            this.formCoin.price =  this.formCoin.price.replace("R$", '')
+                  .replace(',', '.').trim()
+
+            if(this.formCoin.name == '' || this.formCoin.price == '' || this.formCoin.quantity == '') {
+               toastr.error('Todos os campos do formulário são obrigatórios.', 'ERRO')
+                return
+            }
+
+         const coin = {
+               id: this.formCoin.id,
+               name: this.formCoin.name,
+               price: this.formCoin.price,
+               quantity: this.formCoin.quantity
+             }
+
+         const self = this
+
+         axios.put(baseUrl, coin)
+               .then(function (response){
+                toastr.success('Transação atualizada com sucesso!', 'Formulário')
+
+               })
+               .catch(function (error){
+                   toastr.error('Não foi possível atualizar a transação.' + error, 'Formulário')
+               })
+               .then(function () {
+                      self.showAllCoins()
+                      self.showTransactions(self.formCoin.name)
+                      self.cleanForm()
+               })
         }
     }
 };
